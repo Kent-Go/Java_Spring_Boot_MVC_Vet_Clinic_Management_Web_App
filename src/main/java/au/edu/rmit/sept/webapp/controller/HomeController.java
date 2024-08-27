13172008df288dd6314b799a039ec9e05e50af61
Edward3957503 <112810465+Edward3957503@users.noreply.github.com>
2024-08-27 18:23:16 +1000

    feat(register): add initial links to veterinarian and pet owner registration pages

---
+++ src/main/java/au/edu/rmit/sept/webapp/controller/HomeController.java
@@ -0,0 +1,15 @@
+package au.edu.rmit.sept.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("name", "Edward");
+        return "index";  // This will resolve to src/main/resources/templates/index.html
+    }
+}
