5d9bc742ad4b66b9f8bf05e5a4b68079a040582b
eve-s3951140 <s3951140@student.rmit.edu.au>
2024-09-03 13:16:56 +1000

    feat: add UserRepository for database operations and UserAPIController for API endpoints

---
+++ src/main/java/au/edu/rmit/sept/webapp/restcontrollers/UserAPIController.java
@@ -0,0 +1,23 @@
+package au.edu.rmit.sept.webapp.restcontrollers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import au.edu.rmit.sept.webapp.repositories.UserRepository;
+import au.edu.rmit.sept.webapp.models.User;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class UserAPIController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+}
