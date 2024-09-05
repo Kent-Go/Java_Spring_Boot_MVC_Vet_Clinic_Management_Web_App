e9643a96f5ca571e11d75c813c66d7844e656809
eve-s3951140 <s3951140@student.rmit.edu.au>
2024-09-05 15:12:04 +1000

    feat: create model files for address, educational video, medicine, order, payment details, and qualifications

---
+++ src/main/java/au/edu/rmit/sept/webapp/models/Order.java
@@ -0,0 +1,5 @@
+package au.edu.rmit.sept.webapp.models;
+
+public record Order() {
+    
+}
--- src/main/java/au/edu/rmit/sept/webapp/repositories/UserRepository.java
+++ src/main/java/au/edu/rmit/sept/webapp/repositories/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import au.edu.rmit.sept.webapp.models.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByEmail(String email);
 }
