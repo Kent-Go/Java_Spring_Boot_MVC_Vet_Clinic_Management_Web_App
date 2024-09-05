a57f5dd69c603ae8f70fa7c417120256cc1f922b
eve-s3951140 <s3951140@student.rmit.edu.au>
2024-09-05 16:11:08 +1000

    feat: add UserService implementation

---
+++ src/main/java/au/edu/rmit/sept/webapp/services/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package au.edu.rmit.sept.webapp.services;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import au.edu.rmit.sept.webapp.models.User;
+import au.edu.rmit.sept.webapp.repositories.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+  private UserRepository userRepository;
+
+  @Autowired
+  public UserServiceImpl(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  public Collection<User> getAllUsers() {
+    return userRepository.findAll();
+  }
+
+  @Override
+  public User getUserByUserID(int userID) {
+    return userRepository.findById(userID).orElseThrow(() -> new RuntimeException("User not found"));
+  }
+
+  @Override
+  public User createUser(User user) {
+    return userRepository.save(user);
+  }
+
+  @Override 
+  public User getUserByEmail(String email) {
+    return userRepository.findByEmail(email);
+  }
+}
