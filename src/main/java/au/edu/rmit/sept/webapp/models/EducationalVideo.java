d247332b2e799c264dd6e2cf65d75e1a8b447e5d
eve-s3951140 <s3951140@student.rmit.edu.au>
2024-09-09 21:23:08 +1000

    feat(model): add EducationalVideo, Address, and Medicine entity model

--- src/main/java/au/edu/rmit/sept/webapp/models/EducationalVideo.java
+++ src/main/java/au/edu/rmit/sept/webapp/models/EducationalVideo.java
@@ -1,5 +1,48 @@
 package au.edu.rmit.sept.webapp.models;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table(name = "educational_video")
 public class EducationalVideo {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "video_url")
+    private String videoUrl;
+
+    // Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
 }
