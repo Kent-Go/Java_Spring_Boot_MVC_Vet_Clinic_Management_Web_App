0088fa1516f18c2b2f0f68f2bd539626d3db11ae
eve-s3951140 <s3951140@student.rmit.edu.au>
2024-09-09 21:23:36 +1000

    feat(api): add REST controller and repository for retrieving EducationalVideo, Address, and Medicine data

---
+++ src/main/java/au/edu/rmit/sept/webapp/repositories/EducationalVideoRepository.java
@@ -0,0 +1,7 @@
+package au.edu.rmit.sept.webapp.repositories;
+
+import au.edu.rmit.sept.webapp.models.EducationalVideo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EducationalVideoRepository extends JpaRepository<EducationalVideo, Integer> {
+}
---
+++ src/main/java/au/edu/rmit/sept/webapp/restcontrollers/MedicineRestController.java
@@ -0,0 +1,23 @@
+package au.edu.rmit.sept.webapp.restcontrollers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import au.edu.rmit.sept.webapp.models.Medicine;
+import au.edu.rmit.sept.webapp.repositories.MedicineRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/medicine")
+public class MedicineRestController {
+    @Autowired
+    private MedicineRepository medicineRepository;
+
+    @GetMapping
+    public List<Medicine> getMedicines() {
+        return medicineRepository.findAll();
+    }
+}
