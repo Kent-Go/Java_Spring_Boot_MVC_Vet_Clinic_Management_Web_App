81bbcace734ae5ec55995966ad3e4d4e993931f4
eve-s3951140 <s3951140@student.rmit.edu.au>
2024-09-10 17:32:40 +1000

    feat(controller): add pet owner and pet registration handling

--- src/main/java/au/edu/rmit/sept/webapp/services/PetOwnerServiceImpl.java
+++ src/main/java/au/edu/rmit/sept/webapp/services/PetOwnerServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import au.edu.rmit.sept.webapp.models.PetOwner;
 import au.edu.rmit.sept.webapp.repositories.PetOwnerRepository;
-import jakarta.transaction.Transactional;
 
 @Service
 public class PetOwnerServiceImpl implements PetOwnerService {
@@ -29,7 +28,7 @@ public class PetOwnerServiceImpl implements PetOwnerService {
     return petOwnerRepository.findById(userID).orElseThrow(() -> new RuntimeException("Pet Owner not found"));
   }
 
-  @Transactional
+  @Override
   public PetOwner createPetOwner(PetOwner petOwner) {
     return petOwnerRepository.save(petOwner);
   }
