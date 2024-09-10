43ce128c5d8bdf71e6780a1c0bb1443f30207bd9
YoanMario <s3970071@student.rmit.edu.au>
2024-09-10 13:33:24 +1000

    feat(model): Added immunisation history model

---
+++ src/main/java/au/edu/rmit/sept/webapp/models/ImmunisationHistoryDTO.java
@@ -0,0 +1,58 @@
+package au.edu.rmit.sept.webapp.models;
+
+import java.time.LocalDate;
+
+public class ImmunisationHistoryDTO {
+
+    private int id;
+    private String name;
+    private LocalDate date;
+    private String notes;
+    private int petID;
+
+    // Getters and Setters
+    //ID
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    //Name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //Date
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date){
+        this.date = date;
+    }
+
+    //Notes
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    //Pet ID
+    public int getPetID() {
+        return petID;
+    }
+
+    public void setPetID(int petID) {
+        this.petID = petID;
+    }
+}
