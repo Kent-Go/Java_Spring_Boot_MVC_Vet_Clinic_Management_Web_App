5e8cfb1f126c7b3616159e065c2a16ac623ca6f0
rmit-kent-go <s3955624@student.rmit.edu.au>
2024-09-12 11:57:58 +1000

    added display today date and day

--- src/main/java/au/edu/rmit/sept/webapp/controller/VetDashboardController.java
+++ src/main/java/au/edu/rmit/sept/webapp/controller/VetDashboardController.java
@@ -3,12 +3,14 @@ package au.edu.rmit.sept.webapp.controller;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Date;
 
 @Controller
 public class VetDashboardController {
 
     @GetMapping("/vetDashboard")
     public String showVetDashboard(Model model) {
+        model.addAttribute("today", new Date());
         return "vetDashboard"; // Corresponds to vetDashboard.html
     }
 }
