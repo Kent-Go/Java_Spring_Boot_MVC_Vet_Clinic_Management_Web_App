452937fe97f7d25752b61a5687df6f2d38e4e278
eve-s3951140 <s3951140@student.rmit.edu.au>
2024-09-14 21:32:49 +1000

    feat: create repository functions to get a list of qualifications based on vetID

--- src/main/java/au/edu/rmit/sept/webapp/repositories/QualificationRepository.java
+++ src/main/java/au/edu/rmit/sept/webapp/repositories/QualificationRepository.java
@@ -2,10 +2,15 @@ package au.edu.rmit.sept.webapp.repositories;
 
 import au.edu.rmit.sept.webapp.models.Qualification;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface QualificationRepository extends JpaRepository<Qualification, Integer> {
-    Optional<Qualification> findByVetID(int vetID);
+    // Find all qualifications by vetID
+    List<Qualification> findByVetID(int vetID);
+
+    // Find a qualification by id
+    Optional<Qualification> findById(int qualificationID);
 }
