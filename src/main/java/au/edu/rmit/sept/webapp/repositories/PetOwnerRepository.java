f4251d654519f0335de497806ad8226bc67ebc0e
rmit-kent-go <s3955624@student.rmit.edu.au>
2024-09-16 09:02:29 +1000

    added petOwnerId parameter to href link

--- src/main/java/au/edu/rmit/sept/webapp/repositories/PetOwnerRepository.java
+++ src/main/java/au/edu/rmit/sept/webapp/repositories/PetOwnerRepository.java
@@ -1,8 +1,11 @@
 package au.edu.rmit.sept.webapp.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import au.edu.rmit.sept.webapp.models.PetOwner;
 
 public interface PetOwnerRepository extends JpaRepository<PetOwner, Integer> {
+    Optional<PetOwner> findByUserID(int userID);
 }
