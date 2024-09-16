50394f638d9dd0ec7dede78aebe0525600d9f946
rmit-kent-go <s3955624@student.rmit.edu.au>
2024-09-16 20:59:03 +1000

    modify api to request for appointment baed on vet id and date

--- src/main/java/au/edu/rmit/sept/webapp/restcontrollers/AppointmentRestController.java
+++ src/main/java/au/edu/rmit/sept/webapp/restcontrollers/AppointmentRestController.java
@@ -3,6 +3,7 @@ package au.edu.rmit.sept.webapp.restcontrollers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -10,6 +11,7 @@ import au.edu.rmit.sept.webapp.models.Appointment;
 import au.edu.rmit.sept.webapp.repositories.AppointmentRepository;
 
 import java.util.Collection;
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api/appointment")
@@ -18,7 +20,7 @@ public class AppointmentRestController {
     private AppointmentRepository appointmentRepository;
 
     @GetMapping("/{vetID}")
-    public Collection<Appointment> getAppointmentByVetID(@PathVariable("vetID") int vetID) {
-        return appointmentRepository.findByVetID(vetID);
+    public Collection<Appointment> getAppointmentByVetID(@PathVariable("vetID") int vetID, @RequestParam("date") LocalDate date) {
+        return appointmentRepository.findByVetIDAndDateOrderByStartTimeAsc(vetID, date);
     }
 }
