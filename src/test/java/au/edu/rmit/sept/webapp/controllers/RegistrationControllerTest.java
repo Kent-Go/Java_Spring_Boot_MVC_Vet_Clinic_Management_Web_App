a124eb4f9ae37dc1995d32fa7727015abb53e711
eve-s3951140 <s3951140@student.rmit.edu.au>
2024-09-19 23:35:13 +1000

    test: create test case for registration controller

---
+++ src/test/java/au/edu/rmit/sept/webapp/controllers/RegistrationControllerTest.java
@@ -0,0 +1,27 @@
+package au.edu.rmit.sept.webapp.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RegistrationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    // Test for retrieving the registration page
+    @Test
+    public void testShowRegistrationPage() throws Exception {
+        mockMvc.perform(get("/userRegister"))
+                .andExpect(status().isOk()) // Expect HTTP 200 OK status
+                .andExpect(view().name("register")) // Expect the view name to be "register"
+                .andExpect(model().attribute("selected", "")); // Expect model attribute "selected" to be an empty string
+    }
+}
