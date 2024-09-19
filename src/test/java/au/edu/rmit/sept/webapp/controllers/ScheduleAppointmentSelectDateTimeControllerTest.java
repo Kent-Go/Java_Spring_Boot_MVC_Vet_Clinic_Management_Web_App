61e6b07c3a86568c17ecc774d9186dc8a04c9f21
rmit-kent-go <s3955624@student.rmit.edu.au>
2024-09-19 20:55:31 +1000

    added test case for displaying date time page

---
+++ src/test/java/au/edu/rmit/sept/webapp/controllers/ScheduleAppointmentSelectDateTimeControllerTest.java
@@ -0,0 +1,27 @@
+package au.edu.rmit.sept.webapp.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(ScheduleAppointmentSelectDateTimeController.class)
+class ScheduleAppointmentSelectDateTimeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    // Test rendering the appointmentSelectDateTime page
+    void testDisplayDateTime() throws Exception {
+        // Perform a GET request to /appointment/new/select_date_time with vetId and appointmentTypeId parameters
+        mockMvc.perform(get("/appointment/new/select_date_time")
+                .param("vetId", "1")
+                .param("appointmentTypeId", "1"))
+                .andExpect(status().isOk()) // Expect the status to be OK
+                .andExpect(view().name("appointmentSelectDateTime")); // Expect the view to be appointmentSelectDateTime
+    }
+}
