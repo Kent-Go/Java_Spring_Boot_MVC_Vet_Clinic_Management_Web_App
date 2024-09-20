d65b42d5719f849649a6d459166dfe0d3fc87cf8
eve-s3951140 <s3951140@student.rmit.edu.au>
2024-09-20 23:22:46 +1000

    test: create test cases for articles, trends, and videos controller

---
+++ src/test/java/au/edu/rmit/sept/webapp/controllers/VideosControllerTest.java
@@ -0,0 +1,28 @@
+package au.edu.rmit.sept.webapp.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class VideosControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    // Test for retrieving the vidoes page
+    @Test
+    void testShowVideosPage() throws Exception {
+        // Perform GET request to "/trendsHome"
+        mockMvc.perform(get("/videos"))
+                .andExpect(status().isOk()) // Expect HTTP 200 status code
+                .andExpect(view().name("videos")); // Expect the view name to be "videos"
+    }
+
+}
