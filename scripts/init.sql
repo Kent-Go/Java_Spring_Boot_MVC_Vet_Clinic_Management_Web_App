78a1651d3f21f0640cb99993d3ceb026bd4ec10e
eve-s3951140 <s3951140@student.rmit.edu.au>
2024-10-02 14:46:15 +1000

    chore: migration database and app to docker

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,11 +1,13 @@
 -- Drop database
--- DROP DATABASE VetCare;
+DROP DATABASE VetCare;
 
 -- Create database
--- CREATE DATABASE IF NOT EXISTS VetCare;
+CREATE DATABASE IF NOT EXISTS VetCare;
 
----------------- Create Tables ----------------
--- Clinic Address --
+USE VetCare;
+
+-- Create Tables
+-- Clinic Address
 CREATE TABLE IF NOT EXISTS clinic_address (
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	street VARCHAR(255) NOT NULL,
@@ -18,7 +20,7 @@ INSERT INTO clinic_address VALUES
 (2, '109 Bay St', 'Port Melbourne', 'Victoria', '3207'),
 (3, '210 Lygon St', 'Brunswick East', 'Victoria', '3055');
 
--- Clinic --
+-- Clinic
 CREATE TABLE IF NOT EXISTS clinic (
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
@@ -32,7 +34,7 @@ INSERT INTO clinic VALUES
 (2, 'Port Melbourne Veterinary Clinic', 'appointment@portmelbournevet.com', 'portmelbournevet', 2),
 (3, 'Brunswick Central Vet Clinic', 'reception@brunswickcentralvet.com.au', 'brunswickcentralvet', 3);
 
--- User --
+-- User
 CREATE TABLE IF NOT EXISTS user(
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	first_name VARCHAR(255) NOT NULL,
@@ -51,7 +53,7 @@ INSERT INTO user VALUES
 (5, 'Jordan', 'Jackson', '1980-01-01', 'Male', '0109283905', 'jordanS@gmail.com', 'hashedpassword'),
 (6, 'Jeremy', 'John', '1985-10-15', 'Male', '019278920', 'jeremyJ@gmail.com', 'hashedpassword');
 
--- Address --
+-- Address
 CREATE TABLE IF NOT EXISTS address(
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	street VARCHAR(255) NOT NULL,
@@ -69,7 +71,7 @@ INSERT INTO address VALUES
 (5, '196 Nice Avenue', 'Sdyney', 'New South Wales', '3075', 5),
 (6, '1 Drive Street', 'Carlton', 'Victoria', '3053', 6);
 
--- Pet Owner --
+-- Pet Owner
 CREATE TABLE IF NOT EXISTS pet_owner(
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	user_id INT NOT NULL,
@@ -77,7 +79,7 @@ CREATE TABLE IF NOT EXISTS pet_owner(
 );
 INSERT INTO pet_owner VALUES (1, 1), (2, 3);
 
--- Pet Owner Payment Details --
+-- Pet Owner Payment Details
 CREATE TABLE IF NOT EXISTS payment_details (
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
@@ -89,7 +91,7 @@ CREATE TABLE IF NOT EXISTS payment_details (
 );
 INSERT INTO payment_details VALUES (1, 'John Doe', '1234 5678 1234 5678', '12/25', 'hashedCVV1', 1), (2, 'Jane Smith', '8765 4321 8765 4321', '01/26', 'hashedCVV2', 2);
 
--- Pet --
+-- Pet
 CREATE TABLE IF NOT EXISTS pet (
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
@@ -105,7 +107,7 @@ CREATE TABLE IF NOT EXISTS pet (
 );
 INSERT INTO pet VALUES (1, 'Rocky', '2024-05-31', 'Dog', 'Chihuahua', 'Male', 1.5, 'Dust', 'Arthritis',  1), (2, 'Luna', '2023-01-31', 'Dog', 'Chihuahua', 'Female', 2.1, 'Aloe Vera', 'Hip Dysplasia', 1), (3, 'Stella', '2023-01-31', 'Cat', 'Egyptian Mau', 'Female', 2.1, 'Pollens', 'Asthma', 2);
 
--- Immunisation History --
+-- Immunisation History
 CREATE TABLE IF NOT EXISTS immunisation_history (
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
@@ -116,7 +118,7 @@ CREATE TABLE IF NOT EXISTS immunisation_history (
 );
 INSERT INTO immunisation_history VALUES (1, 'HPV', '2023-12-14', '', 1), (2, 'Bordatella', '2023-12-14', '', 1), (3, 'Rabies', '2024-01-17', '', 2), (4, 'Lyme', '2024-01-17', '', 2);
 
--- Surgery History --
+-- Surgery History
 CREATE TABLE IF NOT EXISTS surgery_history (
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
@@ -127,7 +129,7 @@ CREATE TABLE IF NOT EXISTS surgery_history (
 );
 INSERT INTO surgery_history VALUES (1, 'Gastrointestinal Obstructions', '2023-12-14', 'No complications during surgery.', 1), (2, 'Hernia Repair', '2023-12-14', 'Laparoscopic procedure performed.', 2), (3, 'Gastropexy', '2024-01-16', 'Surgery to prevent future gastric dilatation.', 2), (4, 'ACL Repair Surgery', '2024-01-17', 'Patient advised on physiotherapy for recovery.', 2);
 
--- Vet --
+-- Vet
 CREATE TABLE IF NOT EXISTS vet(
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	title VARCHAR(100) NOT NULL,
@@ -145,7 +147,7 @@ INSERT INTO vet VALUES
 (3, 'Dr', 'English, Italian', "Jordan is an internationally recognized expert in animal neurophysiology and the diagnosis of neuromuscular disorders.", 'profilePicLink', 5, 2),
 (4, 'Dr', 'English, Chinese, Malay', "I am dedicated to providing comprehensive and compassionate care to pets of all ages, ensuring their overall well-being and health.", 'profilePicLink', 6, 3);
 
--- Qualification --
+-- Qualification
 CREATE TABLE IF NOT EXISTS qualification(
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(255) NOT NULL,
@@ -158,7 +160,7 @@ CREATE TABLE IF NOT EXISTS qualification(
 INSERT INTO qualification VALUES 
 (1, 'Bachelor of Veterinary Science (BVSc)', 'University of Melbourne', 'Australia', 1997, 1), (2, 'Master of Veterinary Science (MVSc)', 'University of Melbourne', 'Australia', 2002, 1);
 
--- Availability --
+-- Availability
 CREATE TABLE IF NOT EXISTS availability (
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	date DATE NOT NULL,
@@ -167,7 +169,7 @@ CREATE TABLE IF NOT EXISTS availability (
 );
 INSERT INTO availability VALUES (1, '2024-08-31', '13:00', '17:00'), (2, '2024-09-01', '10:00', '14:30'), (3, '2024-09-01', '09:30', '16:30');
 
--- Vet Availability --
+-- Vet Availability
 CREATE TABLE IF NOT EXISTS vet_availability (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
   vet_id INTEGER NOT NULL,
@@ -177,7 +179,7 @@ CREATE TABLE IF NOT EXISTS vet_availability (
 );
 INSERT INTO vet_availability VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2);
 
--- Medicine --
+-- Medicine
 CREATE TABLE IF NOT EXISTS medicine (
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
@@ -186,7 +188,7 @@ CREATE TABLE IF NOT EXISTS medicine (
 );
 INSERT INTO medicine VALUES (1, 'Antinol Plus EAB-277 For Dogs', 1000, 1.5), (2, 'Vetmedin 1ss.25mg Flavoured Chewable Tablets For Dogs', 1500, 0.85), (3, 'Clomicalm 80mg Tablets For Dogs and Cats', 2000, 1.35);
 
--- Appointment Type --
+-- Appointment Type
 CREATE TABLE IF NOT EXISTS appointment_type (
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
@@ -195,7 +197,7 @@ CREATE TABLE IF NOT EXISTS appointment_type (
 );
 INSERT INTO appointment_type VALUES (1, 'General Clinical Consultation', 30, 'This service involves a comprehensive assessment of your pet’s overall health. The veterinarian will discuss any concerns you have, review your pet’s medical history, and provide recommendations for preventive care or treatment.'), (2, 'Physical Examination', 45, 'During a physical examination, the veterinarian will thoroughly check your pet’s body, including the eyes, ears, mouth, skin, and coat. This helps in identifying any signs of illness or abnormalities early on.'), (3, 'Dental Care', 30, 'Dental care services focus on maintaining your pet’s oral health. This includes teeth cleaning, polishing, and addressing any dental issues such as plaque buildup, gum disease, or tooth extractions if necessary.'), (4, 'Surgery', 90, 'Veterinary surgery encompasses a wide range of procedures, from routine spaying and neutering to more complex surgeries like tumor removal or orthopedic operations. These procedures are performed under anesthesia to ensure your pet’s comfort and safety.'), (5, 'Diet and Nutrition', 60, 'This service involves creating a balanced and nutritious diet plan tailored to your pet’s specific needs. The veterinarian will provide guidance on the best types of food, portion sizes, and any necessary supplements to ensure your pet’s optimal health.');
 
--- Clinic Appointment Type Price --
+-- Clinic Appointment Type Price
 CREATE TABLE IF NOT EXISTS clinic_appointment_type_price (
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
     clinic_id INT NOT NULL,
@@ -209,7 +211,7 @@ INSERT INTO clinic_appointment_type_price VALUES
 (6, 2, 1, 100.00), (7, 2, 2, 120.00), (8, 2, 3, 130.00), (9, 2, 4, 3000.00), (10, 2, 5, 75.00),
 (11, 3, 1, 90.00), (12, 3, 2, 130.00), (13, 3, 3, 125.00), (14, 3, 4, 2750.00), (15, 3, 5, 65.00);
 
--- Appointment --
+-- Appointment
 CREATE TABLE IF NOT EXISTS appointment (
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	date DATE NOT NULL,
@@ -224,7 +226,7 @@ CREATE TABLE IF NOT EXISTS appointment (
 );
 INSERT INTO appointment VALUES (1, '2024-08-31', '13:00', '13:30', 1, 1, 1), (2, '2024-08-31', '11:00', '11:30', 1, 1, 1), (3, '2024-09-01', '15:00', '14:00', 2, 2, 2);
 
--- Vet Appointment Type Offered --
+-- Vet Appointment Type Offered
 CREATE TABLE IF NOT EXISTS vet_appointment_type_offered(
 	id INT AUTO_INCREMENT PRIMARY KEY,
     vet_id INTEGER NOT NULL,
