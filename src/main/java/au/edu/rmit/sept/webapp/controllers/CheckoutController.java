f9b51d11232897d2d90d08deaacf58206af6f7da
YoanMario <s3970071@student.rmit.edu.au>
2024-10-10 01:37:33 +1100

    fix: Checkout controller correctly retrieves prescription

--- src/main/java/au/edu/rmit/sept/webapp/controllers/CheckoutController.java
+++ src/main/java/au/edu/rmit/sept/webapp/controllers/CheckoutController.java
@@ -1,35 +1,33 @@
 package au.edu.rmit.sept.webapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import au.edu.rmit.sept.webapp.models.PrescribedMedication;
+import au.edu.rmit.sept.webapp.services.PrescribedMedicationService;
+
 @Controller
 public class CheckoutController {
 
+    @Autowired
+    private PrescribedMedicationService prescriptionService;
+
     @GetMapping("/checkout")
-    public String showCheckoutPage(@RequestParam("prescriptionId") Long prescriptionId, Model model) {
+    public String showCheckoutPage(@RequestParam("prescriptionId") int prescriptionId, Model model) {
 
-        // Dummy data for prescription details
-        model.addAttribute("prescriptionName", "Frusemide 20mg");
-        model.addAttribute("quantity", 1);
-        model.addAttribute("price", 20);
+        PrescribedMedication selectedPrescription = prescriptionService.getPrescribedMedicationByID(prescriptionId);
+        
 
-        model.addAttribute("additionalItemName", "Medicine 2 20mg");
-        model.addAttribute("additionalQuantity", 2);
-        model.addAttribute("additionalPrice", 30);
-
-        // Dummy payment method
-        model.addAttribute("paymentMethod", "MasterCard ending in **34");
-
-        // Dummy shipping address
-        String shippingAddress = "123 Main St, Melbourne, VIC 3000, Australia";
-        model.addAttribute("shippingAddress", shippingAddress);
-
-        // Total price calculation
-        model.addAttribute("totalPrice", 50);
-
+        // Add the selected prescription to the model if found
+        if (selectedPrescription != null) {
+            model.addAttribute("selectedPrescription", selectedPrescription);
+        } else {
+            model.addAttribute("errorMessage", "Prescription not found");
+        }
+        
         return "checkout";
     }
 }
